9a3355d5bf5c442d720ba1d34a6cd4dd92a608b9
Dutra3 <gabrieldutra45@gmail.com>
2023-09-10 21:51:05 -0300

    starting parking lot control api

---
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpot.java
@@ -0,0 +1,46 @@
+package com.api.parkingcontrol.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_parking_spot")
+public class ParkingSpot implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
+
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+
+    @Column(nullable = false, length = 70)
+    private String colorCar;
+
+    @Column(nullable = false)
+    private String registrationDate;
+
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+
+    @Column(nullable = false, length = 30)
+    private String apartment;
+
+    @Column(nullable = false, length = 30)
+    private String block;
+}
